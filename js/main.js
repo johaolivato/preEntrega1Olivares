b2d00907891dc9c2a23e11328cc64dfa831ec11a
Johanna Olivares Toro <johannaolivarestoro@MacBook-Air-de-Johanna.local>
2024-03-13 18:44:41 -0300

    Cambios realizados, sugerencia de tutora Yanina

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ function calcularConCondicional(monto) {
         return;
     }
 
-    var aumento = monto * 0.1375;
-    var valorBruto = monto + aumento;
-    var valorNeto = monto - aumento;
+    let aumento = monto * 0.1375;
+    let valorBruto = monto + aumento;
+    let valorNeto = monto - aumento;
 
     alert("El valor bruto que debes escribir en la boleta es: $" + Math.floor(valorBruto) + "\nEl valor líquido que debes escribir en la boleta es: $" + Math.floor(valorNeto));
 }
 
 // Pedir al usuario el nombre
-var nombre = prompt("¡Hola! ¿Cuál es tu nombre?");
+let nombre = prompt("¡Hola! ¿Cuál es tu nombre?");
 
 // Saludar al usuario
 alert("Hola, " + nombre + "! Bienvenido a la Calculadora Freelance.");
@@ -22,7 +22,7 @@ alert("Hola, " + nombre + "! Bienvenido a la Calculadora Freelance.");
 alert("Recuerda que según SII, desde el 1 de enero de 2024, cada vez que emitas una boleta de honorarios la retención será de 13,75%.");
 
 // Pedir al usuario el monto
-var monto = prompt("Por favor, ingresa el monto que cobraste por tus honorarios.");
+let monto = prompt("Por favor, ingresa el monto que cobraste por tus honorarios.");
 
 // Llamar a la función de cálculo con condicional
 calcularConCondicional(parseFloat(monto));
